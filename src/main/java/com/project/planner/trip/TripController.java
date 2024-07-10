c4777c8b3e6c0db3e52f0b93d702353126c07a73
Mauro-vidal <mauroviidal@gmail.com>
2024-07-10 01:22:23 -0300

    Consulta de dados da viagem.

--- src/main/java/com/project/planner/trip/TripController.java
+++ src/main/java/com/project/planner/trip/TripController.java
@@ -3,11 +3,10 @@ package com.project.planner.trip;
 import com.project.planner.participant.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
@@ -31,6 +30,15 @@ public class TripController {
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
 
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+
+        Optional<Trip> trip = repository.findById(id); // resultado da busca do banco de dados
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build()); //caso encontre uma trip status ok, se for nulo
+                                                                                                // não encontrou um response entity para aquele id
+                                                                                                //vai retornar um status notFound, não encontrado.
     }
 }
